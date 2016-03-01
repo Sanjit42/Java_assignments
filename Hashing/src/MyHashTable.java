53069491e4134af2914902fb744874514fb83d32
Sanjit42 <sanjitprad@gmail.com>
2016-03-02 00:26:39 +0530

    Create removeAll, isPresent functions

--- Hashing/src/MyHashTable.java
+++ Hashing/src/MyHashTable.java
@@ -1,38 +1,39 @@
-import  java.util.ArrayList;
+import java.util.ArrayList;
+
 
 public class MyHashTable {
-    private Object [] values;
-    private Object [] keys;
+    private Object[] values;
+    private Object[] keys;
 
-    private int length;
+    private int length = 0;
     private int boundry;
 
-    public MyHashTable(int size){
-        values = (size % 2 == 0) ?new Object[size+1] : new Object[size];
-        keys = (size % 2 == 0) ?new Object[size+1] : new Object[size];
+    public MyHashTable(int size) {
+        values = (size % 2 == 0) ? new Object[size + 1] : new Object[size];
+        keys = (size % 2 == 0) ? new Object[size + 1] : new Object[size];
         length = 0;
         boundry = size;
     }
 
-    private long hash (String keys){
+    private long hash(String keys) {
         long hash = 7;
-        for (int i = 0; i<keys.length(); i++){
-            hash = (hash * 31 + keys.charAt(i))%boundry;
+        for (int i = 0; i < keys.length(); i++) {
+            hash = (hash * 31 + keys.charAt(i)) % boundry;
         }
         return (int) (Math.abs(hash) % boundry);
     }
 
-    public void put(String key, Object value){
+    public void put(String key, Object value) {
         length++;
         int index = (int) (int) hash(key);
-        if (values[index] == null){
-           values[index] = value;
+        if (values[index] == null) {
+            values[index] = value;
             keys[index] = key;
             return;
         }
         ArrayList innerValues = new ArrayList();
         ArrayList innerKeys = new ArrayList();
-        if (!(values[index] instanceof ArrayList)){
+        if (!(values[index] instanceof ArrayList)) {
             innerValues.add(values[index]);
             innerValues.add(keys[index]);
 
@@ -43,22 +44,34 @@ public class MyHashTable {
         innerKeys.add(key);
     }
 
-    public int getLength(){
+    public Object get(String key) {
+        int index = (int) (int) hash(key);
+        if (values[index] instanceof ArrayList) {
+            return ((ArrayList) values[index]).get(((ArrayList) keys[index]).indexOf(key));
+        }
+        return values[index];
+    }
+
+    public boolean isPresent(String key) {
+        int index = (int) (int) hash(key);
+        return ((keys[index]) != null);
+    }
+
+    public void removeAll(){
+        for (int i = 0; i < boundry ; i++){
+            keys[i] = null;
+            values[i] = null;
+            length = 0;
+        }
+    }
+
+    public int getLength() {
         return length;
     }
-//     public void print(){
-//        for(int i = 0; i < keys.length; i++) {
-//            System.out.println(keys[i] + " " + values[i]);
-//        }
-// }
-//    public static void main(String[] args) {
-//        MyHashTable myHashTable = new MyHashTable(5);
-//        myHashTable.put("India", "dilhi");
-//        myHashTable.put("Indi", "dili");
-//        myHashTable.put("Inda", "dihi");
-//        myHashTable.put("Inia", "dlhi");
-//        myHashTable.put("Idia", "ilhi");
-//        myHashTable.put("ndia", "wdilhi");
-//        myHashTable.print();
-//    }
+
+        public void print() {
+        for (int i = 0; i < keys.length; i++) {
+            System.out.println(keys[i] + " " + values[i]);
+        }
+ }
 }
--- Hashing/test/MyHashTableTest.java
+++ Hashing/test/MyHashTableTest.java
@@ -6,7 +6,54 @@ public class MyHashTableTest {
     @Test
     public void testput_should_put_keys_value_in_the_table() {
         MyHashTable myHashTable = new MyHashTable(3);
-        myHashTable.put("India", "Dilhi");
+        myHashTable.put("India", "Delhi");
         assertEquals(1, myHashTable.getLength());
+    }
+    @Test
+    public void testget_should_get_value_from_the_table_if_give_key () {
+        MyHashTable myHashTable = new MyHashTable(3);
+        myHashTable.put("India", "Delhi");
+        assertEquals("Delhi", myHashTable.get("India"));
+    }
+    @Test
+    public void test_get_values_from_the_table(){
+        MyHashTable myHashTable = new MyHashTable(5);
+        myHashTable.put("India","Delhi");
+        myHashTable.put("Bangaladesh","Dhaka");
+
+        assertEquals("Dhaka", myHashTable.get("Bangaladesh"));
+        assertEquals("Delhi", myHashTable.get("India"));
+        assertEquals(2, myHashTable.getLength());
+    }
+
+    @Test
+    public void test_isPresent_return_true_if_given_key_is_present_in_the_table(){
+        MyHashTable myHashTable = new MyHashTable(3);
+        myHashTable.put("West Bengal", "Kolkata");
+        myHashTable.put("Karnataka", "Bangalore");
+        myHashTable.put("Andhraprodesh", "Hyderabad");
+
+        assertTrue(myHashTable.isPresent("Karnataka"));
+    }
+
+    @Test
+    public void test_isPresent_return_false_if_given_key_not_present_in_the_table(){
+        MyHashTable myHashTable = new MyHashTable(3);
+        myHashTable.put("West Bengal", "Kolkata");
+        myHashTable.put("Maharastra", "Mumbai");
+        myHashTable.put("Karnataka", "Bangalore");
+        assertTrue(myHashTable.isPresent("Karnataka"));
+    }
+
+    @Test
+    public void test_remove_all_value_from_the_table(){
+        MyHashTable myHashTable = new MyHashTable(10);
+        myHashTable.put("West Bengal", "Kolkata");
+        myHashTable.put("Maharastra", "Mumbai");
+        myHashTable.put("Karnataka", "Bangalore");
+        myHashTable.put("Andhraprodesh", "Hyderabad");
+        myHashTable.removeAll();
+
+        assertFalse(myHashTable.isPresent("Karnataka"));
     }
 }
